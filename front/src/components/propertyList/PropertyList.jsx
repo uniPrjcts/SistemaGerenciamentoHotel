6367a8a3e44bb46f1d8108f56612b153ab415d8a
ES <23021423@uepg.br>
2024-09-22 19:42:15 -0300

    ajustes

---
+++ front/src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,40 @@
+import useFetch from "../../hooks/useFetch";
+import "./propertyList.css";
+
+const PropertyList = () => {
+
+    const { data, loading, error } = useFetch("/api/hotels/countByType");
+
+    const images = [
+        "https://www.dicasdeviagem.com/wp-content/uploads/2021/06/hotel-fasano-boa-vista-quarto.jpg",
+        "https://brotas.com.br/wp-content/uploads/2017/08/brotas-eco-resort-01-capa.jpg",
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/c5/2f/35/gemini-hotel.jpg?w=1200&h=-1&s=1",
+        "https://placehold.co/600x400",
+        "https://placehold.co/600x400",
+    ];
+
+    return (
+        <div className="pList">
+            {loading ?
+                ("Loading, por favor espere"
+                ) : (
+                    <>
+                        {data &&
+                            images.map((img, i) => (
+                                <div className="pListItem"  >
+                                    <img
+                                        src={img}
+                                        className="pListImg"
+                                    />
+                                    <div className="pListTitles">
+                                        <h1>{data[i]?.tipo}</h1>
+                                        <h2>{data[i]?.count} {data[i]?.tipo}</h2>
+                                    </div>
+                                </div>
+                            ))}
+                    </>)}
+        </div>
+    );
+};
+
+export default PropertyList;
